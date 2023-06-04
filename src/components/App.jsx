61907832b6630075a041858a7d6fd72a3e0cc108
YuliyaKubar <113610848+YuliyaKubar@users.noreply.github.com>
2023-06-04 19:02:30 +0300

    error in ContactList, Form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,50 @@
+import { useState, useEffect } from 'react';
+import { nanoid } from 'nanoid';
+import { Form } from './Form/Form';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import css from './App.module.css';
+
 export const App = () => {
+  const [contacts, setContacts] = useState(() => {
+    return JSON.parse(localStorage.getItem('contacts'));
+  });
+
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = (name, number) => {
+    if (contacts.find(contact => contact.name === name)) {
+      alert(`${name} is already in contacts.`);
+      return;
+    }
+
+    setContacts(prev => [...prev, { id: nanoid(), name, number }]);
+  };
+
+  const changeFilter = e => {
+    setFilter(e.currentTarget.value);
+  };
+
+  const deleteContact = id => {
+    const filterId = contacts.filter(contact => contact.id !== id);
+    setContacts([...filterId]);
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1 className={css.title}>Phonebook</h1>
+      <Form onSubmit={addContact} />
+      <h2 className={css.contacts}>Contacts</h2>
+      <Filter onChange={changeFilter} value={filter} />
+      <ContactList
+        onClick={deleteContact}
+        contacts={contacts}
+        filter={filter}
+      />
     </div>
   );
 };
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import css from './Filter.module.css';
+
+export const Filter = ({ value, onChange }) => {
+  return (
+    <label>
+      Find contacts by name
+      <input
+        className={css.text}
+        type="text"
+        value={value}
+        onChange={onChange}
+        name="filter"
+      />
+    </label>
+  );
+};
+
+Filter.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.string.isRequired,
+};
