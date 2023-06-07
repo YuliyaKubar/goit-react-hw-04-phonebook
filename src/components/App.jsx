3329f020cf756cd5ae07377224e6dfe863e488a2
YuliyaKubar <113610848+YuliyaKubar@users.noreply.github.com>
2023-06-07 11:08:58 +0300

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,9 +6,15 @@ import { Filter } from './Filter/Filter';
 import css from './App.module.css';
 
 export const App = () => {
-  const [contacts, setContacts] = useState(() => {
-    return JSON.parse(localStorage.getItem('contacts')) ?? [];
-  });
+  const [contacts, setContacts] = useState(
+    () =>
+      JSON.parse(localStorage.getItem('contacts')) ?? [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]
+  );
 
   const [filter, setFilter] = useState('');
 
@@ -19,13 +25,14 @@ export const App = () => {
   const addContact = data => {
     if (
       !contacts.find(
-        contact => data.name.toLocaleLowerCase() === contact.name.toLowerCase()
+        contact => data.name.toLowerCase() === contact.name.toLowerCase()
       )
     ) {
       setContacts(prevState => (prevState ? [...prevState, data] : [data]));
     } else {
       alert(`${data.name} is already in contacts.`);
     }
+    console.log(contacts);
   };
 
   const changeFilter = e => {
